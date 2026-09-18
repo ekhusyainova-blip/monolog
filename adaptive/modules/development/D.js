ecf04b77a7d9abca094144a4e4a25840dcc98f51
ekhusyainova-blip <e.khusyainova@gmail.com>
2026-09-18 23:58:56 +0300

    Update D.js

--- adaptive/modules/development/D.js
+++ adaptive/modules/development/D.js
@@ -37,3 +37,31 @@ window.MonologDevelopment.D = {
     return this.state.currentPath;
   }
 };
+
+// --- дописано: boot-маркер ABCD в /state ---
+// Старый mount выше не трогаем. Только маркеры.
+(function () {
+  if (!window.MonologDevD) window.MonologDevD = {};
+
+  const _mountOld = MonologDevD.mount;
+
+  MonologDevD.markLayers = function () {
+    const layers = ["A", "B", "C", "D"];
+    return Promise.all(
+      layers.map(function (l) {
+        return MonologDevA.state("mount", l, true, "development");
+      })
+    );
+  };
+
+  MonologDevD.mount = function (mountPoint) {
+    const result = _mountOld
+      ? _mountOld.call(MonologDevD, mountPoint)
+      : Promise.resolve();
+    return Promise.resolve(result).then(function () {
+      return MonologDevD.markLayers();
+    }).then(function () {
+      return MonologDevA.boot("ready");
+    });
+  };
+})();
