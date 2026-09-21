f26fd3a7e6f0ef41ee155fff7ae271e2059feb90
ekhusyainova-blip <e.khusyainova@gmail.com>
2026-09-21 20:23:14 +0300

    Create chat_a_code.py

---
+++ chat_a_code.py
@@ -0,0 +1,18 @@
+# chat_a_code.py
+# Тема: code
+# Слой: A (данные)
+# Что: данные о структуре кода
+
+MODULES = [
+    "chat",
+    "ui",
+    "code",
+    "config",
+]
+
+STRUCTURE = {
+    "chat": ["a", "b", "c", "d"],
+    "ui": ["a", "b", "c", "d"],
+    "code": ["a", "b", "c", "d"],
+    "config": ["a", "b", "c", "d"],
+}
