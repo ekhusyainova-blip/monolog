3ec0a8535914cebac06073fa09c30ceb42093c23
ekhusyainova-blip <e.khusyainova@gmail.com>
2026-09-21 20:23:40 +0300

    Create chat_b_code.py

---
+++ chat_b_code.py
@@ -0,0 +1,10 @@
+# chat_b_code.py
+# Тема: code
+# Слой: B (условие)
+# Что: при каких условиях код работает
+
+def when_code_active() -> bool:
+    return True
+
+def which_module(module: str) -> bool:
+    return module in MODULES
